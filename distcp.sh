64ced5d034d31d4510d7cde7778a0198b1d621e3
Russell Jurney <russell.jurney@gmail.com>
2013-05-18 17:46:18 -0700

    Automating distcp at cluster boot

---
+++ distcp.sh
@@ -0,0 +1,5 @@
+hadoop distcp s3://github-explorer/WatchEvent /tmp/WatchEvent
+hadoop distcp s3://github-explorer/ForkEvent /tmp/ForkEvent
+hadoop distcp s3://github-explorer/DownloadEvent /tmp/DownloadEvent
+#hadoop distcp s3://github-explorer/IssuesEvent /tmp/IssuesEvent
+hadoop distcp s3://github-explorer/CreateEvent /tmp/CreateEvent
