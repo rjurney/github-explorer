a8d435ffae1609afb3d81a7e40f9376815505696
Russell Jurney <russell.jurney@gmail.com>
2013-05-22 11:27:15 -0700

    Added script to automate scp up of assets to EMR by hostname

---
+++ scp.sh
@@ -0,0 +1,4 @@
+scp -i ~/.ssh/github-explorer.pem recommend.pig $1:
+scp -i ~/.ssh/github-explorer.pem udfs.py $1:
+scp -i ~/.ssh/github-explorer.pem ~/Software/pig/contrib/piggybank/java/piggybank.jar $1:
+scp -i ~/.ssh/github-explorer.pem ~/Software/datafu/dist/datafu-0.0.9-SNAPSHOT.jar $1:
